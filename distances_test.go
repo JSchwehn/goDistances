1000bb1d423448a2b2aee6ab1b499c9291a235aa
JSchwehn <jschwehn+wit@gmail.com>
2014-12-13 18:38:41 +0100

    Finished the geo coordinate converter

--- distances_test.go
+++ distances_test.go
@@ -3,9 +3,12 @@ package goDistances
 import (
 	"math"
 	"testing"
+	//	"fmt"
 )
+
 var v1 = []float64{4.0, 1.0, -2.0}
 var v2 = []float64{2.0, 3.0, -1.0}
+
 // Euclid distance success
 func TestEclid(t *testing.T) {
 	const out1 = 3.0
@@ -40,7 +43,7 @@ func TestEclidSquare(t *testing.T) {
 		t.Errorf("%v", err)
 	}
 	if !isEqual(distance, out1, precision) {
-		t.Errorf("Distance is wrong. Got %v wanted %v by a precision of %v", distance, out1,precision)
+		t.Errorf("Distance is wrong. Got %v wanted %v by a precision of %v", distance, out1, precision)
 	}
 }
 
@@ -48,7 +51,7 @@ func TestEclidSquare(t *testing.T) {
 func TestNorm(t *testing.T) {
 	const out = 4.05885
 	const precision = 0.00001
-	p 	:= 4.1
+	p := 4.1
 	norm := new(LpNormDistance)
 	distance, err := norm.Distance(v1, p)
 	if err != nil {
@@ -72,45 +75,45 @@ func TestNormTypeMismatch(t *testing.T) {
 func TestManhattanDistance(t *testing.T) {
 	const out = 5.0
 	manhattan := new(ManhattanDistance)
-	distance, err := manhattan.Distance(v1,v2)
+	distance, err := manhattan.Distance(v1, v2)
 	if err != nil {
-		t.Errorf("An unexpected error occured %v",err)
+		t.Errorf("An unexpected error occured %v", err)
 	}
 	if distance != out {
-		t.Errorf("Distance is wrong. Wanted %v - got %v",out, distance)
+		t.Errorf("Distance is wrong. Wanted %v - got %v", out, distance)
 	}
 }
 
 func TestMinkowskiDistanceEclideanLike(t *testing.T) {
 	const out = 3.0
 	minkowskiDistance := new(MinkowskiDistance)
-	distance, err := minkowskiDistance.Distance(v1,v2,2.0)
+	distance, err := minkowskiDistance.Distance(v1, v2, 2.0)
 	if err != nil {
-		t.Errorf("An unexpected error occured %v",err)
+		t.Errorf("An unexpected error occured %v", err)
 	}
 	if distance != out {
-		t.Errorf("Distance is wrong. Wanted %v - got %v",out, distance)
+		t.Errorf("Distance is wrong. Wanted %v - got %v", out, distance)
 	}
 }
 func TestMinkowskiDistanceManhattanLike(t *testing.T) {
 	const out = 5.0
 	minkowskiDistance := new(MinkowskiDistance)
-	distance, err := minkowskiDistance.Distance(v1,v2,1.0)
+	distance, err := minkowskiDistance.Distance(v1, v2, 1.0)
 	if err != nil {
-		t.Errorf("An unexpected error occured %v",err)
+		t.Errorf("An unexpected error occured %v", err)
 	}
 	if distance != out {
-		t.Errorf("Distance is wrong. Wanted %v - got %v",out, distance)
+		t.Errorf("Distance is wrong. Wanted %v - got %v", out, distance)
 	}
 }
 func TestMinkowskiDistanceWeighted(t *testing.T) {
 	const out = 4.24264
 	const precision = 0.00001
-	weights := []float64{2.,2.,2.}
+	weights := []float64{2., 2., 2.}
 	minkowskiDistanceWeighted := new(MinkowskiDistanceWeighted)
-	distance, err := minkowskiDistanceWeighted.Distance(v1,v2,weights,2.0)
+	distance, err := minkowskiDistanceWeighted.Distance(v1, v2, weights, 2.0)
 	if err != nil {
-		t.Errorf("An unexpected error occured %v",err)
+		t.Errorf("An unexpected error occured %v", err)
 	}
 	if !isEqual(distance, out, precision) {
 		t.Errorf("Distance is wrong. Got %v wanted %v by a precision of %v", distance, out, precision)
@@ -120,9 +123,9 @@ func TestChebyshevDistance(t *testing.T) {
 	const out = 2.
 	const precision = 0.00001
 	chebyshevDistance := new(ChebyshevDistance)
-	distance, err := chebyshevDistance.Distance(v1,v2)
+	distance, err := chebyshevDistance.Distance(v1, v2)
 	if err != nil {
-		t.Errorf("An unexpected error occured %v",err)
+		t.Errorf("An unexpected error occured %v", err)
 	}
 	if !isEqual(distance, out, precision) {
 		t.Errorf("Distance is wrong. Got %v wanted %v by a precision of %v", distance, out, precision)
@@ -132,77 +135,231 @@ func TestHammingDistance(t *testing.T) {
 	const out = 3.
 	const precision = 0.00001
 	hammingDistance := new(HammingDistance)
-	distance, err := hammingDistance.Distance(v1,v2)
+	distance, err := hammingDistance.Distance(v1, v2)
 	if err != nil {
-		t.Errorf("An unexpected error occured %v",err)
+		t.Errorf("An unexpected error occured %v", err)
 	}
 	if !isEqual(distance, out, precision) {
 		t.Errorf("Distance is wrong. Got %v wanted %v by a precision of %v", distance, out, precision)
 	}
 }
 func TestBrayCurtisDistance(t *testing.T) {
-	const out       = 0.384615
+	const out = 0.384615
 	const precision = 0.000001
 	brayCurtis := new(BrayCurtisDistance)
-	distance, err := brayCurtis.Distance(v1,v2)
+	distance, err := brayCurtis.Distance(v1, v2)
 	if err != nil {
-		t.Errorf("An unexpected error occured %v",err)
+		t.Errorf("An unexpected error occured %v", err)
 	}
 	if !isEqual(distance, out, precision) {
 		t.Errorf("Distance is wrong. Got %v wanted %v by a precision of %v", distance, out, precision)
 	}
 }
 func TestCanberraDistance(t *testing.T) {
-	const out       = 1.166667
+	const out = 1.166667
 	const precision = 0.000001
 	canberraDistance := new(CanberraDistance)
-	distance, err := canberraDistance.Distance(v1,v2)
+	distance, err := canberraDistance.Distance(v1, v2)
 	if err != nil {
-		t.Errorf("An unexpected error occured %v",err)
+		t.Errorf("An unexpected error occured %v", err)
 	}
 	if !isEqual(distance, out, precision) {
 		t.Errorf("Distance is wrong. Got %v wanted %v by a precision of %v", distance, out, precision)
 	}
 }
+
 // http://www.wolframalpha.com/input/?i=52%C2%B0+31%27+0%22+N+13%C2%B0+24%27+0%22+E+distance+35%C2%B0+42%27+0%22+N+139%C2%B0+46%27+0%22+E
 // http://www.frustfrei-lernen.de/mathematik/bogenmass-und-gradmass.html
 // http://de.wikipedia.org/wiki/Orthodrome#Berechnungsbeispiel_Berlin_.E2.80.93_Tokio
 // http://en.wikipedia.org/wiki/Great-circle_distance
-func TestGeoDistance(t *testing.T) {
+//func TestGeoDistance(t *testing.T) {
+//
+//	const out = 8941
+//	input1 := []float64{52.517, 13.40}
+//	input2 := []float64{35.70, 139.767}
+//	circumstance  := 40000.0 // circumstance of our earth
+//	geoDistance   := new(GeoDistance)
+//	distance, err := geoDistance.Distance(input1, input2, circumstance)
+//
+//	if err != nil {
+//		t.Errorf("An unexpected error occured %v",err)
+//	}
+//	if distance != out {
+//		t.Errorf("Distance is wrong. Got %v wanted %v", distance, out)
+//	}
+//}
 
-	const out = 8941
-	input1 := []float64{52.517, 13.40}
-	input2 := []float64{35.70, 139.767}
-	circumstance  := 40000.0 // circumstance of our earth
-	geoDistance   := new(GeoDistance)
-	distance, err := geoDistance.Distance(input1, input2, circumstance)
+func TestGeoCoordinateToDecimal(t *testing.T) {
+	const input = "52°31'1\"N"
+	const output = 52.516944
+	const precision = 0.0000001
+	d := new(GeoCoordinate)
+	d.ParseDMS(input)
+
+	if res := d.ToDecimal(); isEqual(res, output, precision) {
+		t.Errorf("Converted %v got %v (%2.6f) wanted %v", input, res, res, output)
+	}
+}
+
+func TestDecimalToGeo(t *testing.T) {
+	output := GeoCoordinate{
+		Degree:            52,
+		Minutes:           31,
+		Seconds:           1.3,
+		CardinalDirection: "",
+	}
+	const input = 52.517028
+	d := GeoCoordinate{}
+	d.ParseDecimal(input, 2)
+	if d != output {
+		t.Errorf("Converted %v got %v  wanted %v", input, d, output)
+	}
+}
+
+func TestParseDMS(t *testing.T) {
+	const input_post_n = "52°31'1\"N"
+	output_post_n := GeoCoordinate{
+		Degree:            52,
+		Minutes:           31,
+		Seconds:           1,
+		CardinalDirection: "N",
+	}
+	const input_post_e = "13°24'1\"E"
+	output_post_e := GeoCoordinate{
+		Degree:            13,
+		Minutes:           24,
+		Seconds:           1,
+		CardinalDirection: "E",
+	}
+	const input_post_s = "14°25'2\"S"
+	output_post_s := GeoCoordinate{
+		Degree:            14,
+		Minutes:           25,
+		Seconds:           2,
+		CardinalDirection: "S",
+	}
+	const input_post_w = "14°25'2\"w"
+	output_post_w := GeoCoordinate{
+		Degree:            14,
+		Minutes:           25,
+		Seconds:           2,
+		CardinalDirection: "W",
+	}
+	const input_pre_s = "S15°26'3\""
+	output_pre_s := GeoCoordinate{
+		Degree:            15,
+		Minutes:           26,
+		Seconds:           3,
+		CardinalDirection: "S",
+	}
+	const input_pre_w = "W16°27'4\""
+	output_pre_w := GeoCoordinate{
+		Degree:            16,
+		Minutes:           27,
+		Seconds:           4,
+		CardinalDirection: "W",
+	}
+	const input_pre_n = "n17°28'5\""
+	output_pre_n := GeoCoordinate{
+		Degree:            17,
+		Minutes:           28,
+		Seconds:           5,
+		CardinalDirection: "N",
+	}
+	const input_pre_e = "e18°29'6\""
+	output_pre_e := GeoCoordinate{
+		Degree:            18,
+		Minutes:           29,
+		Seconds:           6,
+		CardinalDirection: "E",
+	}
+
+	d := GeoCoordinate{}
+	var err = d.ParseDMS(input_post_n)
 
 	if err != nil {
-		t.Errorf("An unexpected error occured %v",err)
+		t.Errorf("Unepected Error %v", err)
 	}
-	if distance != out {
-		t.Errorf("Distance is wrong. Got %v wanted %v", distance, out)
+	if d != output_post_n {
+		t.Errorf("Convertion faild. Wanted %v got %v for %v", output_post_n, d, input_post_n)
 	}
+
+	err = d.ParseDMS(input_post_e)
+	if err != nil {
+		t.Errorf("Unepected Error %v", err)
+	}
+	if d != output_post_e {
+		t.Errorf("Convertion faild. Wanted %v got %v for %v", output_post_e, d, input_post_e)
+	}
+
+	err = d.ParseDMS(input_post_s)
+	if err != nil {
+		t.Errorf("Unepected Error %v", err)
+	}
+	if d != output_post_s {
+		t.Errorf("Convertion faild. Wanted %v got %v for %v", output_post_s, d, input_post_s)
+	}
+
+	err = d.ParseDMS(input_post_w)
+	if err != nil {
+		t.Errorf("Unepected Error %v", err)
+	}
+	if d != output_post_w {
+		t.Errorf("Convertion faild. Wanted %v got %v for %v", output_post_w, d, input_post_w)
+	}
+
+	err = d.ParseDMS(input_pre_s)
+	if err != nil {
+		t.Errorf("Unepected Error %v", err)
+	}
+	if d != output_pre_s {
+		t.Errorf("Convertion faild. Wanted %v got %v for %v", output_pre_s, d, input_pre_s)
+	}
+
+	err = d.ParseDMS(input_pre_w)
+	if err != nil {
+		t.Errorf("Unepected Error %v", err)
+	}
+	if d != output_pre_w {
+		t.Errorf("Convertion faild. Wanted %v got %v for %v", output_pre_w, d, input_pre_w)
+	}
+
+	err = d.ParseDMS(input_pre_n)
+	if err != nil {
+		t.Errorf("Unepected Error %v", err)
+	}
+	if d != output_pre_n {
+		t.Errorf("Convertion faild. Wanted %v got %v for %v", output_pre_n, d, input_pre_n)
+	}
+
+	err = d.ParseDMS(input_pre_e)
+	if err != nil {
+		t.Errorf("Unepected Error %v", err)
+	}
+	if d != output_pre_e {
+		t.Errorf("Convertion faild. Wanted %v got %v for %v", output_pre_e, d, input_pre_e)
+	}
+
 }
 
 func TestConvertDMSToDegrees(t *testing.T) {
-	const inputLati = "52°31'1\"N"
-	const inputLong = "13° 24' 1\" E"
-	const outLati = 3.2
-	const outLong = -64.1
-	const precision = 0.000001
-	g := new(GeoDistance)
-	result := g.ConvertDMSToDegrees(inputLati, inputLong)
-	if ! isEqual(result[0], outLati, precision) {
-		t.Errorf("We got for lantitude %v but wanted %v", result[0], outLati)
-	}
-	if ! isEqual(result[1], outLong, precision) {
-		t.Errorf("We got for lantitude %v but wanted %v", result[1], outLong)
-	}
+	//	const inputLati = "52°31'1\"N"
+	//	const inputLong = "13° 24' 1\" E"
+	//	const outLati = 3.2
+	//	const outLong = -64.1
+	//	const precision = 0.000001
+	//	g := new(GeoDistance)
+	//	result := g.ConvertDMSToDegrees(inputLati, inputLong)
+	//	if ! isEqual(result[0], outLati, precision) {
+	//		t.Errorf("We got for lantitude %v but wanted %v", result[0], outLati)
+	//	}
+	//	if ! isEqual(result[1], outLong, precision) {
+	//		t.Errorf("We got for lantitude %v but wanted %v", result[1], outLong)
+	//	}
 }
 
 func isEqual(f1, f2 float64, tolerance float64) bool {
-	if math.Abs((f1-f2)) <= tolerance {
+	if math.Abs((f1 - f2)) <= tolerance {
 		return true
 	}
 	return false
--- geoDistance.go
+++ geoDistance.go
@@ -2,38 +2,82 @@ package goDistances
 
 import (
 	"fmt"
-//	"math"
+	"math"
+	"regexp"
+	"strconv"
+	"strings"
 )
 
 // GeoDistance calculates the distance
-type GeoDistance struct {}
-//type GeoCoordinate struct {
-//	degree  float64
-//	minutes float64
-//	seconds float64
-//	ns string // n(orth) or (s)outh
-//	ew string // e(ast) or w(est)
-//}
+type GeoDistance struct{}
+type GeoCoordinate struct {
+	Degree            float64
+	Minutes           float64
+	Seconds           float64
+	CardinalDirection string // n(orth),(s)outh, (e)ast or (w)est
+}
+
+func (gC GeoCoordinate) ToDMS() string {
+	return fmt.Sprintf("%s %f°%f'%f\"", gC.CardinalDirection, gC.Degree, gC.Minutes, gC.Seconds)
+}
+func (gC GeoCoordinate) ToDecimal() float64 {
+	decimal := gC.Degree
+	decimal += gC.Minutes / 60
+	decimal += gC.Seconds / 3600
+	return decimal
+}
+func (gC *GeoCoordinate) ParseDecimal(d float64, round int) error {
+	gC.Degree = math.Floor(d)
+	leftover := (d - gC.Degree) * 60
+	gC.Minutes = math.Floor(leftover)
+	gC.Seconds = gC.round((leftover-gC.Minutes)*60, 2)
+	return nil
+}
+func (gC *GeoCoordinate) ParseDMS(dms string) error {
+	dmsCoordinate := regexp.MustCompile("^([NSEW])*\\s*(\\d+)\\s*°\\s*(\\d+)\\s*'\\s*(\\d+)\\s*\"\\s*([NSEW])*")
+	if match := dmsCoordinate.FindStringSubmatch(strings.ToUpper(dms)); match != nil {
+		// detect the cardinal direction char
+		if match[5] != "" {
+			gC.CardinalDirection = match[5]
+		} else {
+			gC.CardinalDirection = match[1]
+		}
+		gC.Degree, _ = strconv.ParseFloat(match[2], 64)
+		gC.Minutes, _ = strconv.ParseFloat(match[3], 64)
+		gC.Seconds, _ = strconv.ParseFloat(match[4], 64)
+		return nil
+	}
+	return fmt.Errorf("Could not parse given dms coordinates %v", dms)
+}
+func (gC GeoCoordinate) round(val float64, places int) (newVal float64) {
+	var round float64
+	roundOn := .5
+	pow := math.Pow(10, float64(places))
+	digit := pow * val
+	_, div := math.Modf(digit)
+	if div >= roundOn {
+		round = math.Ceil(digit)
+	} else {
+		round = math.Floor(digit)
+	}
+	newVal = round / pow
+	return
+}
 
 // Distance takes two float64 slices which have to have two elements each.
 // The first parameter of the slice  will be considered as north angle and the second
 // one will be considered as east angle.
 // It will return a float for the distance between the two slices.
-func (mG GeoDistance) Distance(params ...interface{}) (float64,error) {
+func (mG GeoDistance) Distance(params ...interface{}) (float64, error) {
 	if len(params) != 3 {
-		return -1.,  fmt.Errorf("wrong parameter count. Needed 3 got %d", len(params))
+		return -1., fmt.Errorf("wrong parameter count. Needed 3 got %d", len(params))
 	}
 	d1 := params[0].([]float64) // latitude *longitute
 	d2 := params[1].([]float64) //
 	c := params[2].(float64)
 
-	fmt.Printf("We got %v %v %v  \n",d1, d2, c)
+	fmt.Printf("We got %v %v %v  \n", d1, d2, c)
 	d := 0.0
 
 	return d, nil
-}
-
-func (mG GeoDistance) ConvertDMSToDegrees(dmsLati, dmsLong string) ([]float64){
-	fmt.Printf("DMS: %v %v ",dmsLati, dmsLong)
-	return []float64{1.,2.}
 }
